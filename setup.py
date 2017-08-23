916c1fa1731192e740f683bcccdaf74a71917f84
Dustin Ingram <di@users.noreply.github.com>
2017-08-23 18:19:55 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+print('You are on branch `master`')
+
+setup(name='git_ref_example',
+      version='0.1',
+      description='',
+      url='http://github.com/di/git_ref_example',
+      author='Dustin Ingram',
+      author_email='di@promptworks.com',
+      license='MIT',
+      packages=['git_ref_example'],
+      zip_safe=False)
